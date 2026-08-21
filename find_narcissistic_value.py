ca83099de3043ee3e57152bdb922b88c815bf3e0
Muhammad Usman Tahir <umalik1019@gmail.com>
2026-08-22 00:56:42 +0500

    Add instructions for Narcissistic Number calculation
    Added instructions for identifying Narcissistic Numbers.

--- find_narcissistic_value.py
+++ find_narcissistic_value.py
@@ -1,4 +1,5 @@
 '''
+Instructions:
 A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. 
 We will restrict ourselves to decimal (base 10).
 
