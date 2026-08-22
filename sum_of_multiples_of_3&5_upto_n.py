571fc2157e9e5ff30021c591c89e720275987525
Muhammad Usman Tahir <umalik1019@gmail.com>
2026-08-22 19:40:06 +0500

    Create sum_of_multiples_of_3&5_upto_n.py

---
+++ sum_of_multiples_of_3&5_upto_n.py
@@ -0,0 +1,11 @@
+'''
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+Additionally, if the number is negative, return 0.
+
+Note: If a number is a multiple of both 3 and 5, only count it once.
+'''
+
+def solution(number):
+    return sum(n for n in range(number) if (n % 3 == 0 or n % 5 == 0)) 
+  # loop from 0 to number by checking each number multiple of 3 and 5 in n, and them sum
